d525dfe5a584b3fde8bd675772f9b2e58cf17add
Joseph Alfredo <jalfredopoly@gmail.com>
2018-05-29 17:50:07 -0700

    client side setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Cookery</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
